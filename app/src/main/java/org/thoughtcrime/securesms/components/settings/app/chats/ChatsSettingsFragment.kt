e441b27a70a8d890976e2e200d7123980b2877de
Unknown <johanw@xs4all.nl>
2021-06-05 15:47:45 +0200

    Added extra options
    - Encrypted and plaintext backup / restore.
    - Import SMS database. Removed apk expire.
    - Choose between passphrase protection and the Android screenlock.
    - Choice for the backup location (internal or removable storage).
    - Set the maptype in the place picker.
    - Option to treat view-once media as normal media.
    - Option to ignore remote deletion.
    - Choose between FCM or websocket notification delivery.
    - Option to delete only the media from a message, not the rest of the message.
    - Enabled internal preferences in advanced preferences. Use at your own risk.

--- app/src/main/java/org/thoughtcrime/securesms/components/settings/app/chats/ChatsSettingsFragment.kt
+++ app/src/main/java/org/thoughtcrime/securesms/components/settings/app/chats/ChatsSettingsFragment.kt
@@ -2,6 +2,7 @@ package org.thoughtcrime.securesms.components.settings.app.chats
 
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import androidx.preference.PreferenceManager // JW: added
 import org.thoughtcrime.securesms.R
 import org.thoughtcrime.securesms.components.settings.DSLConfiguration
 import org.thoughtcrime.securesms.components.settings.DSLSettingsAdapter
@@ -9,13 +10,18 @@ import org.thoughtcrime.securesms.components.settings.DSLSettingsFragment
 import org.thoughtcrime.securesms.components.settings.DSLSettingsText
 import org.thoughtcrime.securesms.components.settings.configure
 
+
 class ChatsSettingsFragment : DSLSettingsFragment(R.string.preferences_chats__chats) {
 
   private lateinit var viewModel: ChatsSettingsViewModel
 
+  private val mapLabels by lazy { resources.getStringArray(R.array.pref_map_type_entries) } // JW: added
+  private val mapValues by lazy { resources.getStringArray(R.array.pref_map_type_values) }  // JW: added
+
   override fun bindAdapter(adapter: DSLSettingsAdapter) {
     val repository = ChatsSettingsRepository()
-    val factory = ChatsSettingsViewModel.Factory(repository)
+    val preferences = PreferenceManager.getDefaultSharedPreferences(requireContext()) // JW: added
+    val factory = ChatsSettingsViewModel.Factory(preferences, repository) // JW: added preferences
     viewModel = ViewModelProviders.of(this, factory)[ChatsSettingsViewModel::class.java]
 
     viewModel.state.observe(viewLifecycleOwner) {
@@ -80,6 +86,84 @@ class ChatsSettingsFragment : DSLSettingsFragment(R.string.preferences_chats__ch
         summary = DSLSettingsText.from(if (state.chatBackupsEnabled) R.string.arrays__enabled else R.string.arrays__disabled),
         onClick = {
           Navigation.findNavController(requireView()).navigate(R.id.action_chatsSettingsFragment_to_backupsPreferenceFragment)
+        }
+      )
+
+      // JW: added
+      switchPref(
+        title = DSLSettingsText.from(R.string.preferences_chats__chat_backups_removable),
+        summary = DSLSettingsText.from(R.string.preferences_chats__backup_chats_to_removable_storage),
+        isChecked = state.chatBackupsLocation,
+        onClick = {
+          viewModel.setChatBackupLocation(!state.chatBackupsLocation)
+        }
+      )
+
+      // JW: added
+      switchPref(
+        title = DSLSettingsText.from(R.string.preferences_chats__chat_backups_zipfile),
+        summary = DSLSettingsText.from(R.string.preferences_chats__backup_chats_to_encrypted_zipfile),
+        isChecked = state.chatBackupZipfile,
+        onClick = {
+          viewModel.setChatBackupZipfile(!state.chatBackupZipfile)
+        }
+      )
+
+      // JW: added
+      switchPref(
+        title = DSLSettingsText.from(R.string.preferences_chats__chat_backups_zipfile_plain),
+        summary = DSLSettingsText.from(R.string.preferences_chats__backup_chats_to_encrypted_zipfile_plain),
+        isChecked = state.chatBackupZipfilePlain,
+        onClick = {
+          viewModel.setChatBackupZipfilePlain(!state.chatBackupZipfilePlain)
+        }
+      )
+
+      dividerPref()
+
+      sectionHeaderPref(R.string.preferences_chats__control_message_deletion)
+
+      // JW: added
+      switchPref(
+        title = DSLSettingsText.from(R.string.preferences_chats__chat_keep_view_once_messages),
+        summary = DSLSettingsText.from(R.string.preferences_chats__keep_view_once_messages_summary),
+        isChecked = state.keepViewOnceMessages,
+        onClick = {
+          viewModel.keepViewOnceMessages(!state.keepViewOnceMessages)
+        }
+      )
+
+      // JW: added
+      switchPref(
+        title = DSLSettingsText.from(R.string.preferences_chats__chat_ignore_remote_delete),
+        summary = DSLSettingsText.from(R.string.preferences_chats__chat_ignore_remote_delete_summary),
+        isChecked = state.ignoreRemoteDelete,
+        onClick = {
+          viewModel.ignoreRemoteDelete(!state.ignoreRemoteDelete)
+        }
+      )
+
+      // JW: added
+      switchPref(
+        title = DSLSettingsText.from(R.string.preferences_chats__delete_media_only),
+        summary = DSLSettingsText.from(R.string.preferences_chats__delete_media_only_summary),
+        isChecked = state.deleteMediaOnly,
+        onClick = {
+          viewModel.deleteMediaOnly(!state.deleteMediaOnly)
+        }
+      )
+
+      dividerPref()
+
+      sectionHeaderPref(R.string.preferences_chats__google_map_type)
+
+      // JW: added
+      radioListPref(
+        title = DSLSettingsText.from(R.string.preferences__map_type),
+        listItems = mapLabels,
+        selected = mapValues.indexOf(state.googleMapType),
+        onSelected = {
+          viewModel.setGoogleMapType(mapValues[it])
         }
       )
     }
