7c71f86b63390329006f43545fe78f9b1601598d
Unknown <johanw@xs4all.nl>
2022-03-01 20:04:34 +0100

    Added extra options
    - Encrypted and plaintext backup / restore.
    - Import SMS database. Removed apk expire.
    - Import unencrypted WhatsApp database.
    - Choose between passphrase protection and the Android screenlock.
    - Choice for the backup location (internal or removable storage).
    - Set the maptype in the place picker.
    - Option to treat view-once media as normal media.
    - Option to ignore remote deletion.
    - Choose between FCM or websocket notification delivery.
    - Option to delete only the media from a message, not the rest of the message.
    - Removed forward limit.
    - Enabled internal preferences. Use at your own risk.
    - Added an option to auto-leave groups if you are added by a blocked contact.

--- app/src/main/java/org/thoughtcrime/securesms/components/settings/app/privacy/advanced/AdvancedPrivacySettingsViewModel.kt
+++ app/src/main/java/org/thoughtcrime/securesms/components/settings/app/privacy/advanced/AdvancedPrivacySettingsViewModel.kt
@@ -81,6 +81,12 @@ class AdvancedPrivacySettingsViewModel(
     refresh()
   }
 
+  // JW: added
+  fun setPushNotificationsViaFCM(enabled: Boolean) {
+    SignalStore.account().fcmEnabled = enabled
+    refresh()
+  }
+
   fun refresh() {
     store.update { getState().copy(showProgressSpinner = it.showProgressSpinner) }
   }
@@ -109,6 +115,9 @@ class AdvancedPrivacySettingsViewModel(
         ApplicationDependencies.getApplication()
       ),
       false
+      // JW: added
+      ,
+      pushNotificationsViaFCM = SignalStore.account().fcmEnabled
     )
   }
 
