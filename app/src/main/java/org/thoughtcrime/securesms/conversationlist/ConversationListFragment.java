b9be563679ac243361384654c1a952c08da42fa0
Johan <johanw@xs4all.nl>
2022-09-14 23:28:41 +0200

    Added extra options
    - Encrypted and plaintext backup / restore.
    - Import SMS database. Removed apk expire.
    - Import unencrypted WhatsApp database.
    - Choose between passphrase protection and the Android screenlock.
    - Choice for the backup location (internal or removable storage).
    - Set the maptype in the place picker.
    - Option to treat view-once media as normal media.
    - Option to ignore remote deletion.
    - Choose between FCM or websocket notification delivery.
    - Option to delete only the media from a message, not the rest of the message.
    - Removed forward limit.
    - Enabled internal preferences. Use at your own risk.
    - Added options to select who can add you to a group.

--- app/src/main/java/org/thoughtcrime/securesms/conversationlist/ConversationListFragment.java
+++ app/src/main/java/org/thoughtcrime/securesms/conversationlist/ConversationListFragment.java
@@ -79,6 +79,7 @@ import org.signal.core.util.DimensionUnit;
 import org.signal.core.util.concurrent.SignalExecutors;
 import org.signal.core.util.concurrent.SimpleTask;
 import org.signal.core.util.logging.Log;
+import org.thoughtcrime.securesms.ImportExportActivity; // JW
 import org.thoughtcrime.securesms.MainFragment;
 import org.thoughtcrime.securesms.MainNavigator;
 import org.thoughtcrime.securesms.MuteDialog;
@@ -472,6 +473,7 @@ public class ConversationListFragment extends MainFragment implements ActionMode
         handleMarkAllRead(); return true;
       case R.id.menu_invite:
         handleInvite(); return true;
+      case R.id.menu_import_export: handleImportExport(); return true; // JW: added
       case R.id.menu_insights:
         handleInsights(); return true;
       case R.id.menu_notification_profile:
@@ -479,6 +481,11 @@ public class ConversationListFragment extends MainFragment implements ActionMode
     }
 
     return false;
+  }
+
+  // JW: added
+  private void handleImportExport() {
+    startActivity(new Intent(requireActivity(), ImportExportActivity.class));
   }
 
   private boolean isSearchOpen() {
