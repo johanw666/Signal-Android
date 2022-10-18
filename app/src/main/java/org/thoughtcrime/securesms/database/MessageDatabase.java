1fee4332000334f2d2f00bb12fd1d5191cb1cdb2
Johan <johanw@xs4all.nl>
2022-10-18 23:58:40 +0200

    Added extra options
    - Encrypted and plaintext backup / restore.
    - Import SMS database. Removed apk expire.
    - Import unencrypted WhatsApp database.
    - Choose between passphrase protection and the Android screenlock.
    - Choice for the backup location (internal or removable storage).
    - Set the maptype in the place picker.
    - Option to treat view-once media as normal media.
    - Option to ignore remote deletion.
    - Choose between FCM or websocket notification delivery.
    - Option to delete only the media from a message, not the rest of the message.
    - Removed forward limit.
    - Enabled internal preferences. Use at your own risk.
    - Added options to select who can add you to a group.

--- app/src/main/java/org/thoughtcrime/securesms/database/MessageDatabase.java
+++ app/src/main/java/org/thoughtcrime/securesms/database/MessageDatabase.java
@@ -100,6 +100,7 @@ public abstract class MessageDatabase extends Database implements MmsSmsColumns,
   public abstract Set<Long> getAllRateLimitedMessageIds();
   public abstract Cursor getUnexportedInsecureMessages(int limit);
   public abstract void deleteExportedMessages();
+  public abstract void resetExportedMessages(); // JW: added
 
   public abstract void markExpireStarted(long messageId);
   public abstract void markExpireStarted(long messageId, long startTime);
@@ -400,6 +401,17 @@ public abstract class MessageDatabase extends Database implements MmsSmsColumns,
 
   public int getUnexportedInsecureMessagesCount(long threadId) {
     try (Cursor cursor = getWritableDatabase().query(getTableName(), SqlUtil.COUNT, getInsecureMessageClause(threadId) + " AND " + EXPORTED + " < ?", SqlUtil.buildArgs(MessageExportStatus.EXPORTED), null, null, null)) {
+      if (cursor.moveToFirst()) {
+        return cursor.getInt(0);
+      }
+    }
+
+    return 0;
+  }
+
+  // JW: added
+  public int getExportedInsecureMessagesCount() {
+    try (Cursor cursor = getWritableDatabase().query(getTableName(), SqlUtil.COUNT, getInsecureMessageClause() + " AND " + EXPORTED, null, null, null, null)) {
       if (cursor.moveToFirst()) {
         return cursor.getInt(0);
       }
