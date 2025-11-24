58683bf4b598449a76461c1ad707e8ed462d2e1a
Johan <johanw@xs4all.nl>
2025-11-24 20:25:50 +0100

    Added extra options
    - Encrypted and plaintext (xml) backup / restore.
    - Removed apk expire.
    - Import unencrypted WhatsApp database (this option might be outdated).
    - Choose between passphrase protection and the Android screenlock.
    - Choice for the backup location: for Android 4 - 10, internal or removable storage; for Android 11+ any directory can be chosen.
    - Set the maptype in the place picker.
    - Option to treat view-once media as normal media.
    - Option to ignore remote deletion.
    - Choose between FCM or websocket notification delivery.
    - Option to delete only the media from a message, not the rest of the message. This does not remove the message or media on linked devices.
    - Removed forward limit.
    - Enabled internal preferences. Use at your own risk.
    - Added options to select who can add you to a group.
    - Added backup tier override in the online backup settings.

--- app/src/main/java/org/thoughtcrime/securesms/main/MainToolbar.kt
+++ app/src/main/java/org/thoughtcrime/securesms/main/MainToolbar.kt
@@ -103,6 +103,7 @@ interface MainToolbarCallback {
   fun onCloseActionModeClick()
   fun onSearchQueryUpdated(query: String)
   fun onNotificationProfileTooltipDismissed()
+  fun onImportExportClick() // JW
 
   object Empty : MainToolbarCallback {
     override fun onNewGroupClick() = Unit
@@ -124,6 +125,7 @@ interface MainToolbarCallback {
     override fun onCloseActionModeClick() = Unit
     override fun onSearchQueryUpdated(query: String) = Unit
     override fun onNotificationProfileTooltipDismissed() = Unit
+    override fun onImportExportClick() = Unit // JW
   }
 }
 
@@ -370,7 +372,6 @@ private fun PrimaryToolbar(
           modifier = Modifier
             .fillMaxSize()
             .clickable(
-              enabled = enabled,
               onClick = callback::onSettingsClick,
               interactionSource = interactionSource,
               indication = ripple(radius = 14.dp)
@@ -581,6 +582,19 @@ private fun CallDropdownItems(callFilter: CallLogFilter, callback: MainToolbarCa
     )
   }
 
+  // JW: added
+  DropdownMenus.Item(
+    text = {
+      Text(
+        text = stringResource(R.string.arrays__import_export_orig)
+      )
+    },
+    onClick = {
+      callback.onImportExportClick()
+      onOptionSelected()
+    }
+  )
+
   DropdownMenus.Item(
     text = {
       Text(
@@ -683,6 +697,19 @@ private fun ChatDropdownItems(state: MainToolbarState, callback: MainToolbarCall
       }
     )
   }
+
+  // JW: added
+  DropdownMenus.Item(
+    text = {
+      Text(
+        text = stringResource(R.string.arrays__import_export_orig)
+      )
+    },
+    onClick = {
+      callback.onImportExportClick()
+      onOptionSelected()
+    }
+  )
 
   DropdownMenus.Item(
     text = {
