064fa706c712da4fd8e3995b1ed973b36b92b7b6
Johan <johanw@xs4all.nl>
2026-02-20 11:01:58 +0100

    Added extra options
    - Encrypted and plaintext (xml) backup / restore.
    - Removed apk expire.
    - Import unencrypted WhatsApp database (this option might be outdated).
    - Choose between passphrase protection and the Android screenlock.
    - Choice for the backup location: for Android 4 - 10, internal or removable storage; for Android 11+ any directory can be chosen.
    - Set the maptype in the place picker.
    - Option to treat view-once media as normal media.
    - Option to ignore remote deletion.
    - Choose between FCM or websocket notification delivery.
    - Option to delete only the media from a message, not the rest of the message. This does not remove the message or media on linked devices.
    - Removed forward limit.
    - Enabled internal preferences. Use at your own risk.
    - Added options to select who can add you to a group.
    - Added backup tier override in the online backup settings.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,10 +20,19 @@ plugins {
 
 apply(from = "static-ips.gradle.kts")
 
-val canonicalVersionCode = 1654
-val canonicalVersionName = "8.0.2"
-val currentHotfixVersion = 0
+val canonicalVersionCode = 1670
+val canonicalVersionName = "8.0.2.0-JW"
+val currentHotfixVersion = 1
 val maxHotfixVersions = 100
+
+// JW: re-added
+val abiPostFix: Map<String, Int> = mapOf(
+  "universal" to 0,
+  "armeabi-v7a" to 1,
+  "arm64-v8a" to 2,
+  "x86" to 3,
+  "x86_64" to 4
+)
 
 // We don't want versions to ever end in 0 so that they don't conflict with nightly versions
 val possibleHotfixVersions = (0 until maxHotfixVersions).toList().filter { it % 10 != 0 }
@@ -210,8 +219,8 @@ android {
 
     manifestPlaceholders["mapsKey"] = "AIzaSyCSx9xea86GwDKGznCAULE9Y5a8b-TfN9U"
 
-    buildConfigField("long", "BUILD_TIMESTAMP", getLastCommitTimestamp() + "L")
-    buildConfigField("String", "GIT_HASH", "\"${getGitHash()}\"")
+    buildConfigField("long", "BUILD_TIMESTAMP", "1000L") // JW: fixed time for reproducible builds, is not used anyway
+    buildConfigField("String", "GIT_HASH", "\"000000\"") // JW
     buildConfigField("String", "SIGNAL_URL", "\"https://chat.signal.org\"")
     buildConfigField("String", "STORAGE_URL", "\"https://storage.signal.org\"")
     buildConfigField("String", "SIGNAL_CDN_URL", "\"https://cdn.signal.org\"")
@@ -258,7 +267,7 @@ android {
     buildConfigField("String", "STRIPE_BASE_URL", "\"https://api.stripe.com/v1\"")
     buildConfigField("String", "STRIPE_PUBLISHABLE_KEY", "\"pk_live_6cmGZopuTsV8novGgJJW9JpC00vLIgtQ1D\"")
     buildConfigField("boolean", "TRACING_ENABLED", "false")
-    buildConfigField("boolean", "LINK_DEVICE_UX_ENABLED", "false")
+    buildConfigField("boolean", "LINK_DEVICE_UX_ENABLED", "true") // JW
     buildConfigField("boolean", "USE_STRING_ID", "false")
 
     ndk {
@@ -319,8 +328,10 @@ android {
 
     getByName("release") {
       isMinifyEnabled = true
+      manifestPlaceholders["mapsKey"] = getMapsKey() // JW
       proguardFiles(*buildTypes["debug"].proguardFiles.toTypedArray())
       buildConfigField("String", "BUILD_VARIANT_TYPE", "\"Release\"")
+      buildConfigField("boolean", "LINK_DEVICE_UX_ENABLED", "true") // JW
     }
 
     create("instrumentation") {
@@ -487,6 +498,26 @@ android {
     lintConfig = rootProject.file("lint.xml")
   }
 
+  // JW added
+  applicationVariants.all {
+    outputs
+      .map { it as com.android.build.gradle.internal.api.ApkVariantOutputImpl }
+      .forEach { output ->
+        // JW: rewrote section
+        output.outputFileName = output.outputFileName.replace(".apk", "-$versionName.apk")
+
+        val abiName: String = output.getFilter("ABI") ?: "universal"
+        val postFix: Int = abiPostFix[abiName]!!
+
+        if (postFix >= maxHotfixVersions) {
+          throw AssertionError("maxHotfixVersions is too large")
+        }
+
+        output.versionCodeOverride = canonicalVersionCode * maxHotfixVersions + postFix
+      }
+  }
+  // End JW: added
+
   androidComponents {
     beforeVariants { variant ->
       variant.enable = variant.name in selectableVariants
@@ -499,9 +530,10 @@ android {
 
       // Starting with minSdk 23, Android leaves native libraries uncompressed, which is fine for the Play Store, but not for our self-distributed APKs.
       // This reverts it to the legacy behavior, compressing the native libraries, and drastically reducing the APK file size.
-      if (variant.name.contains("website", ignoreCase = true) || variant.name.contains("github", ignoreCase = true)) {
+      // JW: Always use it
+      //if (variant.name.contains("website", ignoreCase = true) || variant.name.contains("github", ignoreCase = true)) {
         variant.packaging.jniLibs.useLegacyPackaging.set(true)
-      }
+      //}
 
       // Version overrides
       if (variant.name.contains("nightly", ignoreCase = true)) {
@@ -567,7 +599,7 @@ android {
   applicationVariants.configureEach {
     outputs.configureEach {
       if (this is com.android.build.gradle.internal.api.BaseVariantOutputImpl) {
-        outputFileName = outputFileName.replace(".apk", "-$versionName.apk")
+        //outputFileName = outputFileName.replace(".apk", "-$versionName.apk") // JW
       }
     }
   }
@@ -612,6 +644,7 @@ dependencies {
   implementation(project(":feature:camera"))
   implementation(project(":feature:registration"))
 
+  implementation("net.lingala.zip4j:zip4j:2.11.6") // JW: added
   implementation(libs.androidx.fragment.ktx)
   implementation(libs.androidx.appcompat) {
     version {
@@ -773,18 +806,22 @@ tasks.withType<Test>().configureEach {
 }
 
 fun getLastCommitTimestamp(): String {
-  return providers.exec {
-    commandLine("git", "log", "-1", "--pretty=format:%ct")
-  }.standardOutput.asText.get() + "000"
+  return "\"000000\"" // JW
+  //return providers.exec {
+  //  commandLine("git", "log", "-1", "--pretty=format:%ct")
+  //}.standardOutput.asText.get() + "000"
 }
 
 fun getGitHash(): String {
-  return providers.exec {
-    commandLine("git", "rev-parse", "HEAD")
-  }.standardOutput.asText.get().trim().substring(0, 12)
+  return "\"000000\"" // JW
+  //return providers.exec {
+  //  commandLine("git", "rev-parse", "HEAD")
+  //}.standardOutput.asText.get().trim().substring(0, 12)
 }
 
 fun getNightlyTagForCurrentCommit(): String? {
+  return "\"000000\"" // JW
+/*
   val output = providers.exec {
     commandLine("git", "tag", "--points-at", "HEAD")
   }.standardOutput.asText.get().trim()
@@ -795,23 +832,30 @@ fun getNightlyTagForCurrentCommit(): String? {
   } else {
     null
   }
+*/
 }
 
 fun getNightlyBuildNumber(tag: String?): Int {
+  return 0 // JW
+/*
   if (tag == null) {
     return 0
   }
 
   val match = Regex("-(\\d{3})$").find(tag)
   return match?.groupValues?.get(1)?.toIntOrNull() ?: 0
+*/
 }
 
+// JW: rewrote function
 fun getMapsKey(): String {
-  return providers
-    .gradleProperty("mapsKey")
-    .orElse(providers.environmentVariable("MAPS_KEY"))
-    .orElse("AIzaSyCSx9xea86GwDKGznCAULE9Y5a8b-TfN9U")
-    .get()
+  val mapKey = file("${project.rootDir}/maps.key")
+
+  return if (mapKey.exists()) {
+    mapKey.readLines()[0]
+  } else {
+    "AIzaSyCSx9xea86GwDKGznCAULE9Y5a8b-TfN9U"
+  }
 }
 
 abstract class LanguageListValueSource : ValueSource<List<String>, LanguageListValueSource.Params> {
