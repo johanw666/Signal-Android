80b06f2f5b52f2a60f25b4a8e0581a902ad08247
Cody Henthorne <cody@signal.org>
2026-08-24 13:12:02 -0400

    Fix internal recipient search fragment dialog insets.

--- app/src/main/java/org/thoughtcrime/securesms/components/settings/app/internal/search/InternalSearchFragment.kt
+++ app/src/main/java/org/thoughtcrime/securesms/components/settings/app/internal/search/InternalSearchFragment.kt
@@ -12,8 +12,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalContentColor
@@ -62,6 +65,7 @@ fun InternalSearchFragmentScreen(query: String, results: ImmutableList<InternalS
       modifier = modifier
         .fillMaxWidth()
         .background(backgroundColor)
+        .windowInsetsPadding(WindowInsets.safeDrawing)
     ) {
       item(key = -1) {
         SearchBar(query, onSearchUpdated)
